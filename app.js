6b2aeaaf5d1aa2dc01b539f61488f0378269a50c
Muhammad Areeb <m.areebkhan125@gmail.com>
2021-08-04 01:54:57 -0800

    StopWatch

---
+++ app.js
@@ -0,0 +1,50 @@
+let min=0;
+let sec=0;
+let miliSec=0;
+
+
+let getMin=document.getElementById("min")
+let getSec=document.getElementById("sec")
+let getMiliSec=document.getElementById("miliSec");
+
+function timer(){
+    miliSec++;
+    getMiliSec.innerHTML=miliSec;
+    if(miliSec >= 100){
+        sec++;
+        getSec.innerHTML=sec;
+        miliSec=0;
+    }
+    else if(sec >= 60){
+        min++;
+        getMin.innerHTML=min;
+        sec=0;
+    }
+
+}
+let setInt;
+function startTimer(){
+     setInt=setInterval(timer,10);
+    let disable=document.getElementById("start")
+     disable.disabled=true;
+};
+
+function stopTimer(){
+    clearInterval(setInt)
+    let enable=document.getElementById("start")
+    enable.disabled=false;
+}
+
+function resetTimer(){
+    min=0;
+    sec=0;
+    miliSec=0;
+    getMin.innerHTML=min;
+    getSec.innerHTML=sec;
+    getMiliSec.innerHTML=miliSec
+    clearInterval(setInt)
+    let enable=document.getElementById("start")
+    enable.disabled=false;
+}
+
+
