170ca794663614040e6c63d5336505f417ac5c95
Muhammad Areeb <m.areebkhan125@gmail.com>
2021-08-12 03:44:38 -0800

    changing

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ let min=0;
 let sec=0;
 let miliSec=0;
 
-let getHr=document.getElementById("hr")
-let getMin=document.getElementById("min")
-let getSec=document.getElementById("sec")
+let getHr=document.getElementById("hr");
+let getMin=document.getElementById("min");
+let getSec=document.getElementById("sec");
 let getMiliSec=document.getElementById("miliSec");
 
 function timer(){
@@ -25,9 +25,9 @@ function timer(){
         hr++;
         getHr.innerHTML=hr;
         min=0;
-    }
+    };
 
-}
+};
 let setInt;
 function startTimer(){
     setInt=setInterval(timer,10); 
@@ -38,17 +38,15 @@ function stopTimer(){
     clearInterval(setInt)
     let enable=document.getElementById("start")
     enable.disabled=false;
-}
+};
 function resetTimer(){
-    hr=0;
     min=0;
     sec=0;
     miliSec=0;
-    getHr.innerHTML=hr;
     getMin.innerHTML=min;
     getSec.innerHTML=sec;
     getMiliSec.innerHTML=miliSec
     clearInterval(setInt)
     let enable=document.getElementById("start")
     enable.disabled=false;
-}
+};
